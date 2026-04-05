2f3b2031c4454777c4bd0640c4906d0dc7ceb842
jpadden01 <96446425+jpadden01@users.noreply.github.com>
2026-04-04 19:32:25 -0700

    Fix typo

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ function operate(num1, num2, operator) {
     switch (operator) {
         case "+":
             return add(num1, num2);
-        case "-":
+        case "−":
             return subtract(num1, num2);
         case "×":
             return multiply(num1, num2);
