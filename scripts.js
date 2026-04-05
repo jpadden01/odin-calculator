cb04140f06ac9165d231100cf409a40ec6537969
jpadden01 <96446425+jpadden01@users.noreply.github.com>
2026-04-04 18:39:04 -0700

    Add logic that updates number on button press

--- scripts.js
+++ scripts.js
@@ -2,6 +2,9 @@ let num1;
 let operator;
 let num2;
 
+let displayValue = document.querySelector(".display-value");
+let numberButtons = document.querySelectorAll(".number, .zero");
+
 function operate(num1, num2, operator) {
     switch (operator) {
         case "add":
@@ -31,3 +34,19 @@ function divide(num1, num2) {
     if (num2 === 0) return "Can not divide by zero"
     return num1 / num2;
 }
+
+function updateNumber(num) {
+    if (operator === undefined) {
+        num1 = (num1 === undefined)? 
+            num
+            : Number(num1.toString() + num);
+        displayValue.textContent = num1;
+    } else {
+        num2 = (num2 === undefined)? 
+            num
+            : Number(num2.toString() + num);
+        displayValue.textContent = num2;
+    }
+}
+
+numberButtons.forEach((cur) => cur.addEventListener("click", () => updateNumber(cur.textContent)));
